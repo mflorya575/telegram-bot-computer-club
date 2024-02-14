301c19db1b4f724f4d79127fe9c4cec4ad82f802
Michael Florya <mih2303@mail.ru>
2024-02-14 16:43:30 +0300

    удалены старые фотографии услуг, добавлены новые фотографии, модифицирован файл Category.py, где были вписаны фото

--- handlers/Category.py
+++ handlers/Category.py
@@ -11,45 +11,45 @@ async def costs(message):
 
 
 async def gta5(call):
-    with open('files/media/manikur.jpg', "rb") as img:
+    with open('files/media/gta5.jpeg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_gta5, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
 
 
 async def rdr2(call):
-    with open('files/media/pedikur.jpg', "rb") as img:
+    with open('files/media/rdr2.jpg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_rdr2, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
 
 
 async def sims4(call):
-    with open('files/media/narast.png', "rb") as img:
+    with open('files/media/sims4.jpg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_sims4, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
 
 async def battlefield4(call):
-    with open('files/media/narast.png', "rb") as img:
+    with open('files/media/battlefield4.jpeg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_battlefield, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
 
 async def atomic_heart(call):
-    with open('files/media/narast.png', "rb") as img:
+    with open('files/media/atomic_heart.jpg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_atomic_heart, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
 
 async def cyberpunk(call):
-    with open('files/media/narast.png', "rb") as img:
+    with open('files/media/cyberpunk.jpeg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_cyberpunk, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
 
 async def roblox(call):
-    with open('files/media/narast.png', "rb") as img:
+    with open('files/media/roblox.jpg', "rb") as img:
         mes = InputMediaPhoto(media=img, caption=texts.category.game_roblox, parse_mode='HTML')
         await call.message.edit_media(mes, reply_markup=buy_kb)
     await call.answer()
